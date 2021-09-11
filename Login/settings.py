1756f4d459eee7e4971d97f49775633383b99938
Teknath-jha <62249788+Teknath-jha@users.noreply.github.com>
2021-09-11 18:37:05 +0530

    Update settings.py

--- Login/settings.py
+++ Login/settings.py
@@ -131,10 +131,10 @@ STATIC_URL = '/static/'
 #      os.path.join(BASE_DIR,'static')
 #  ]
 # media file willupload here 
-# MEDIA_URL="/media/"
-# MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
