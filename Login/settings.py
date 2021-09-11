24ea9df32d6e085c5d3c09e0298b6b9a2052f16a
Teknath-jha <62249788+Teknath-jha@users.noreply.github.com>
2021-09-11 18:34:40 +0530

    udate static urs | settings

--- Login/settings.py
+++ Login/settings.py
@@ -131,7 +131,13 @@ STATIC_URL = '/static/'
 #      os.path.join(BASE_DIR,'static')
 #  ]
 # media file willupload here 
-MEDIA_URL="/media/"
-MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
+# MEDIA_URL="/media/"
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
