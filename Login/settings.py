2f11f63666f2220cef094e1fdd67742d6a488498
Teknath-jha <62249788+Teknath-jha@users.noreply.github.com>
2021-09-11 18:42:48 +0530

    revert back to payment

--- Login/settings.py
+++ Login/settings.py
@@ -139,6 +139,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
